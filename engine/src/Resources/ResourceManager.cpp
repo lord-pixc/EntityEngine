6081e6c9a9ae4a15bbcc23735da6c3ce52f36ad0
lord-pixc <eliancorea2317@gmail.com>
2026-09-12 00:30:31 -0600

    feat(resources): stub de ResourceManager::Load/Unload

---
+++ engine/src/Resources/ResourceManager.cpp
@@ -0,0 +1,16 @@
+#include "../../include/Resources/ResourceManager.h"
+
+namespace EntityEngine
+{
+
+    TextureHandle ResourceManager::Load(const std::string &filePath)
+    {
+        
+        return TextureHandle(); // Retorna un handle vacío por ahora
+    }
+
+    void ResourceManager::Unload(const std::string &filePath)
+    {
+    }
+
+} // namespace EntityEngine
